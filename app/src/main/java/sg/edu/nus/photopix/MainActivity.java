6189bf54aa160a0d2bf0e1982f1bce9fab1885fd
Luo Shaohuai <marksh8@gmail.com>
2015-04-12 21:28:45 +0800

    fin without capture button

--- app/src/main/java/sg/edu/nus/photopix/MainActivity.java
+++ app/src/main/java/sg/edu/nus/photopix/MainActivity.java
@@ -1,19 +1,25 @@
 package sg.edu.nus.photopix;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
